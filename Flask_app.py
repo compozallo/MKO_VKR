7c2ac4b3494bbd569d7d9c74d2734331c88917f7
compozallo <kuzaduza18@gmail.com>
2025-05-16 21:30:57 +0300

    colors +

--- Flask_app.py
+++ Flask_app.py
@@ -50,7 +50,7 @@ def create_main_plot_json():
                 mode='markers+text',
                 marker=dict(size=14, color='red', symbol='circle'),
                 name='Целевая точка',
-                text=["Перетащи меня"],
+                text=["Перетащить"],
                 textposition="top center"
             ))
 
