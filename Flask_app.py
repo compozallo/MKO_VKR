035b2b406810ee53c8d72890f714fceec3137937
compozallo <kuzaduza18@gmail.com>
2025-05-18 18:20:27 +0300

    +local

--- Flask_app.py
+++ Flask_app.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 optimizer = ParetoOptimizer()
 selected_point = None
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+#port = int(os.environ.get("PORT", 5000))
+#app.run(host="0.0.0.0", port=port)
 
-#def get_local_ip():
-    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #try:
-        #s.connect(('10.255.255.255', 1))
-        #IP = s.getsockname()[0]
-    #except:
-        #IP = '127.0.0.1'
-    #finally:
-        #s.close()
-    #return IP
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 @app.route('/move_point', methods=['POST'])
 def move_point():
@@ -91,8 +91,8 @@ def index():
     )
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-    #ip = get_local_ip()
-    #print(f"Запущено на http://{ip}:1500")
-    #app.run(host=ip, port=1500, debug=True)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host="0.0.0.0", port=port, debug=True)
+    ip = get_local_ip()
+    print(f"Запущено на http://{ip}:1500")
+    app.run(host=ip, port=1500, debug=True)
